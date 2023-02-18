886e9873d96c6eb19fd6a78f480d4a0f59ebbd55
hectoremilio1000 <hectoremilio1000@gmail.com>
2023-02-18 02:28:15 -0600

    inicio

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>La llorona</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
