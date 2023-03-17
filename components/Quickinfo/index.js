c4051c967238459dc652a25b7576696fd8083095
hectoremilio1000 <hectoremilio1000@gmail.com>
2023-03-17 02:48:57 -0600

    cambios deploy

--- components/Quickinfo/index.js
+++ components/Quickinfo/index.js
@@ -32,6 +32,9 @@ function QuickInfo() {
         </p>
       </div>
 
+      <div sytle={{ maxWidth: 100}}>
+
+     
       <div className="max-w-3xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-10">
         <div className="grid grid-cols-1 gap-y-10 sm:grid-cols-2 gap-x-6 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
           <div className="w-full aspect-w-1 aspect-h-1 bg-gray-200 rounded-lg overflow-hidden xl:aspect-w-7 xl:aspect-h-8">
@@ -89,7 +92,7 @@ function QuickInfo() {
           
         </div>
       </div>
-
+      </div>
       {/* <div className={card.cardContainer}
       >
         <Link href="/menullorona/Alimentos">
--- pages/_app.js
+++ pages/_app.js
@@ -2,7 +2,6 @@ import "../styles/main.css";
 import "../styles/global.css"
 import "../components/Navbar/navBar.css"
 import LayoutFinal from "../components/layout";
-import 'antd/dist/antd';
 
 
 export default function MyApp({ Component, pageProps }) {
