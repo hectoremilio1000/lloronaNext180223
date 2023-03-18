aaf7ea86846c6a5980d780e3b35135411398ee7f
jampierzcode <jampierv127@gmail.com>
2023-03-17 20:11:15 -0500

    ultimo cambio para llorona

--- components/TitlebarImageListFranquicias/index.js
+++ components/TitlebarImageListFranquicias/index.js
@@ -1,21 +1,16 @@
-import Image from 'next/image';
 import React from 'react'
 import title from "../Main/title.module.css"
 
-import image1 from '../../public/images/franquicia/IMG_0059.jpg'
-import image2 from '../../public/images/franquicia/IMG_0062.jpg'
-import image3 from '../../public/images/franquicia/IMG_0063.jpg'
-import image4 from '../../public/images/franquicia/IMG_0071.jpg'
-
-const images = [
-  { id: 1, img: "/images/franquicia/IMG_0059.jpg", alt: 'imagen1' },
-  { id: 2, img: "/images/franquicia/IMG_0062.jpg", alt: 'imagen2' },
-  { id: 3, img: "/images/franquicia/IMG_0063.jpg", alt: 'imagen1' },
-  { id: 4, img: "/images/franquicia/IMG_0071.jpg", alt: 'imagen2' },
-];
-
 
 function TitlebarImageListFranquicias() {
+  const image1 = "https://imagenesrutalab.s3.amazonaws.com/llorona/nextImage/IMG_0059+(1).jpg"
+  const image2 = "https://imagenesrutalab.s3.amazonaws.com/llorona/nextImage/IMG_0062+(1).jpg"
+  const image3 = "https://imagenesrutalab.s3.amazonaws.com/llorona/nextImage/IMG_0063+(1).jpg"
+  const image4 = "https://imagenesrutalab.s3.amazonaws.com/llorona/nextImage/IMG_0071+(1).jpg"
+  const image5 = "https://imagenesrutalab.s3.amazonaws.com/llorona/nextImage/lloronaDentro+(2).jpg"
+  const image6 = "https://imagenesrutalab.s3.amazonaws.com/llorona/nextImage/lugarAfuera+(2).jpg"
+  const image7 = "https://imagenesrutalab.s3.amazonaws.com/llorona/nextImage/menu%CC%81Veggie+(1).jpg"
+  const image8 = "https://imagenesrutalab.s3.amazonaws.com/llorona/nextImage/mixologi%CC%81a%2Bautor+(1).jpg"
   return (
     <div>
       <div>
@@ -29,25 +24,25 @@ function TitlebarImageListFranquicias() {
       <div className="max-w-2xl mx-auto py-4 px-4 sm:py-4 sm:px-6 lg:max-w-7xl lg:px-8">
         <div className="grid grid-cols-1 gap-y-10 sm:grid-cols-2 gap-x-6 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
           <div className="w-full aspect-w-1 aspect-h-1 bg-gray-200 rounded-lg overflow-hidden xl:aspect-w-7 xl:aspect-h-8">
-            <Image
+            <img style={{ objectFit: "cover" }}
               alt=""
               src={image1}
-            
+              objectFit="cover"
               className="group-hover:opacity-75"
             />
 
           </div>
           <div className="w-full aspect-w-1 aspect-h-1 bg-gray-200 rounded-lg overflow-hidden xl:aspect-w-7 xl:aspect-h-8">
-            <Image
+            <img style={{ objectFit: "cover" }}
               alt=""
               src={image2}
-             
+              objectFit="cover"
               className="group-hover:opacity-75"
             />
 
           </div>
           <div className="w-full aspect-w-1 aspect-h-1 bg-gray-200 rounded-lg overflow-hidden xl:aspect-w-7 xl:aspect-h-8">
-            <Image
+            <img style={{ objectFit: "cover" }}
               alt=""
               src={image3}
               layout="fill"
@@ -57,7 +52,47 @@ function TitlebarImageListFranquicias() {
 
           </div>
           <div className="w-full aspect-w-1 aspect-h-1 bg-gray-200 rounded-lg overflow-hidden xl:aspect-w-7 xl:aspect-h-8">
-            <Image
+            <img style={{ objectFit: "cover" }}
+              alt=""
+              src={image5}
+              layout="fill"
+              objectFit="cover"
+              className="group-hover:opacity-75"
+            />
+
+          </div>
+          <div className="w-full aspect-w-1 aspect-h-1 bg-gray-200 rounded-lg overflow-hidden xl:aspect-w-7 xl:aspect-h-8">
+            <img style={{ objectFit: "cover" }}
+              alt=""
+              src={image6}
+              layout="fill"
+              objectFit="cover"
+              className="group-hover:opacity-75"
+            />
+
+          </div>
+          <div className="w-full aspect-w-1 aspect-h-1 bg-gray-200 rounded-lg overflow-hidden xl:aspect-w-7 xl:aspect-h-8">
+            <img style={{ objectFit: "cover" }}
+              alt=""
+              src={image7}
+              layout="fill"
+              objectFit="cover"
+              className="group-hover:opacity-75"
+            />
+
+          </div>
+          <div className="w-full aspect-w-1 aspect-h-1 bg-gray-200 rounded-lg overflow-hidden xl:aspect-w-7 xl:aspect-h-8">
+            <img style={{ objectFit: "cover" }}
+              alt=""
+              src={image8}
+              layout="fill"
+              objectFit="cover"
+              className="group-hover:opacity-75"
+            />
+
+          </div>
+          <div className="w-full aspect-w-1 aspect-h-1 bg-gray-200 rounded-lg overflow-hidden xl:aspect-w-7 xl:aspect-h-8">
+            <img style={{ objectFit: "contain" }}
               alt=""
               src={image4}
               layout="fill"
