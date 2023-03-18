dc1020a6ce0b52096f88a96fa48e697fae909bc0
hectoremilio1000 <hectoremilio1000@gmail.com>
2023-03-18 12:11:53 -0600

    cambios portada

--- pages/index.js
+++ pages/index.js
@@ -21,15 +21,14 @@ export default function Home() {
         <link rel="apple-touch-icon" href="../logo192.png" />
         <link rel="manifest" href="../manifest.json" />
       </Head>
-      <div className="d-flex subContenedor1Banner banner bannerHome">
+      <div className="d-flex subContenedor1Banner bannerFranquicias banner">
+
         {/* <video autoPlay muted loop src={videoPort} /> */}
 
         <div className="d-flex w-44 sm:w-60 md:w-80 mx-2 title-portada">
           <img src={logo2} width={450}
-            alt="llorona" className="mt-5"  />
-          {/* <h3 className=" font-weight-bold text-3xl md:text-4xl uppercase font-black text-center text-white">
-            Mexican Experience
-          </h3> */}
+            alt="llorona" className="mt-5" />
+          </div>
 
           {/* <div className="subSubDiv1Banner">
             <Link href="/reserva">
@@ -38,8 +37,28 @@ export default function Home() {
               </button>
             </Link>
           </div> */}
-        </div>
+      
       </div>
+
+      {/* <div className="d-flex subContenedor1Banner banner bannerHome">
+        <video autoPlay muted loop src={videoPort} /> 
+
+        <div className="d-flex w-44 sm:w-60 md:w-80 mx-2 title-portada">
+          <img src={logo2} width={450}
+            alt="llorona" className="mt-5"  />
+           <h3 className=" font-weight-bold text-3xl md:text-4xl uppercase font-black text-center text-white">
+            Mexican Experience
+          </h3>
+
+           <div className="subSubDiv1Banner">
+            <Link href="/reserva">
+              <button type="button" className="buttonComponente focus:outline-none text-white bg-green-400 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">
+                ¡Reserva ya!
+              </button>
+            </Link>
+          </div>
+        </div>
+      </div> */}
       {/* <MenuDetail /> */}
       {/* <Promociones /> */}
       <QuickInfo />
