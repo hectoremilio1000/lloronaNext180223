5e942efa9087b7aa67e60bc5dc0672eb08772e3d
hectoremilio1000 <hectoremilio1000@gmail.com>
2023-06-04 16:25:08 -0600

    cambios tag google

--- pages/_app.js
+++ pages/_app.js
@@ -9,15 +9,13 @@ import { useEffect } from 'react'
 import Script from 'next/script'
 import { useRouter } from 'next/router'
 import * as fbq from '../lib/fpixel';
+import { GTM_ID, pageview } from '../lib/gtm'
 
 
 
 export default function MyApp({ Component, pageProps }) {
 
-  
-  const pixelId = fbq?.FB_PIXEL_ID;
 
-  console.log(pixelId);
 
   const router = useRouter()
 
@@ -32,6 +30,13 @@ export default function MyApp({ Component, pageProps }) {
     router.events.on('routeChangeComplete', handleRouteChange)
     return () => {
       router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [router.events])
+
+  useEffect(() => {
+    router.events.on('routeChangeComplete', pageview)
+    return () => {
+      router.events.off('routeChangeComplete', pageview)
     }
   }, [router.events])
 
@@ -55,8 +60,19 @@ export default function MyApp({ Component, pageProps }) {
           `,
         }}
       />
-
-
+      <Script
+        id="gtag-base"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer', '${GTM_ID}');
+          `,
+        }}
+      />
     </Head>
  
     <AppContextProvider>
--- pages/_document.js
+++ pages/_document.js
@@ -1,5 +1,6 @@
 import { Html, Head, Main, NextScript } from "next/document";
-import { FB_PIXEL_ID } from '../lib/fpixel'
+import { FB_PIXEL_ID } from '../lib/fpixel';
+import { GTM_ID, pageview } from '../lib/gtm'
 
 export default function Document() {
   return (
@@ -11,6 +12,12 @@ export default function Document() {
             width="1"
             style={{ display: 'none' }}
             src={`https://www.facebook.com/tr?id=${FB_PIXEL_ID}&ev=PageView&noscript=1`}
+          />
+          <iframe
+            src={`https://www.googletagmanager.com/ns.html?id=${GTM_ID}`}
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
           />
         </noscript>
         
