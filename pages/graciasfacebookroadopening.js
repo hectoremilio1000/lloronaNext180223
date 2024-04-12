0392b68b17cc034e149f3c7ab3987c0367f31a8b
hectoremilio1000 <hectoremilio1000@gmail.com>
2024-04-12 00:13:40 -0600

    evento

---
+++ pages/graciasfacebookroadopening.js
@@ -0,0 +1,31 @@
+import Link from "next/link";
+import React from "react";
+
+const Graciasfacebookroadopening = () => {
+  return (
+    <div className="h-[100vh] w-full bg-black py-[120px] flex flex-col justify-center items-center fixed z-[6000] left-0 right-0">
+      <div className="w-full max-w-[1184px] mx-auto flex flex-col items-center">
+        <h1 className="text-4xl text-white text-center">
+          Gracias por tu reserva
+        </h1>
+        <p className="text-xl text-white text-center mx-auto max-w-[700px]">
+          Gracias por elegir Cantina La Llorona para el evento roadopening! Nos
+          llena de alegría saber que has decidido reservar una mesa con
+          nosotros. Estamos emocionados de poder brindarte una velada
+          inolvidable llena de deliciosos sabores y momentos especiales. <br />{" "}
+          Tu reserva ha sido confirmada con éxito. Prepárate para disfrutar de
+          auténticos platillos mexicanos en un ambiente cálido y acogedor.{" "}
+          <br />
+          Si necesitas realizar alguna modificación a tu reserva o tienes alguna
+          pregunta, nuestro equipo está aquí para ayudarte. No dudes en ponerte
+          en contacto con nosotros.
+        </p>
+        <Link className="px-4 py-3 bg-white font-bold rounded" href={"/"}>
+          Volver al inicio
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default Graciasfacebookroadopening;
---
+++ pages/roadopeninggoogleads.js
@@ -0,0 +1,197 @@
+import Head from "next/head";
+import NavBar from "../components/NavBarEs/NavBarEs";
+import ModalAll from "../components/ModalAll";
+import React, { useEffect, useState } from "react";
+import { InlineWidget } from "react-calendly";
+import CalendarioTiktokTree from "../components/CalendarioTiktokTree";
+import { useAppContext } from "../components/context/Context";
+
+function RoadopeningGoogleAds() {
+  const [open, setOpen] = useState(false);
+  const { espa } = useAppContext();
+  return (
+    <>
+      <Head>
+        <meta
+          property="og:title"
+          content="Experiencia culinaria en cdmx 🥃"
+        ></meta>
+        <meta
+          property="og:description"
+          content="Disfruta de una experiencia culinaria en cdmx"
+        ></meta>
+        <title>
+          Mezcal artesanal y Cómodo lugar en México | Cantina la llorona
+        </title>
+      </Head>
+      {espa ? (
+        <>
+          <NavBar />
+          <div className="pb-4">
+            <div
+              className="d-flex subContenedor1Banner"
+              style={{
+                backgroundImage: `url("https://imagenesrutalab.s3.amazonaws.com/llorona/nextImage/aguacatechicharronrobeye.jpg")`,
+                backgroundSize: "cover",
+                height: "80vh",
+                backgroundPosition: "right center",
+                position: "relative",
+              }}
+            ></div>
+          </div>
+          <div className="container-llorona">
+            <div>
+              <h4 className="text-3xl text-center font-bold uppercase">
+                ROAD TO OPENING
+              </h4>
+            </div>
+          </div>
+          <div className="flex flex-col lg:flex-row lg:items-center lg:justify-center lg:p-8">
+            <div className="lg:w-1/4">
+              <p className="w-full max-w-[1000px] mx-auto text-center md:text-justify sm:text-2xl text-lg text-black font-bold">
+                Primer acto
+              </p>
+              <p className="w-full max-w-[1000px]  mx-auto text-center md:text-justify sm:text-base text-sm text-black">
+                Domingo 14 abril 2024. <br />
+                5:00 PM A 9:00 PM
+              </p>
+            </div>
+            <div className="lg:w-3/4">
+              {" "}
+              <p className="w-full max-w-[800px]  mx-auto text-justify sm:text-2xl text-xl text-black text-bold ml-4 sm:text-lg text-sm">
+                Tenemos el gusto de invitarte a que degustes platillos que hemos
+                seleccionado para ti de nuestro nuevo menú, acompañados de
+                nuestro bartender invitado y de dj rasnyshh.
+              </p>
+            </div>
+          </div>
+          <div className="flex flex-col lg:flex-row lg:items-center lg:justify-between lg:p-8 mt-4">
+            <div className="lg:w-1/2 flex justify-center">
+              <div class="max-w-sm rounded overflow-hidden shadow-lg">
+                <img
+                  className="w-64 h-64 object-cover"
+                  src="https://imagenesrutalab.s3.amazonaws.com/llorona/nextImage/jorgedumit.jpg"
+                  alt="Sunset in the mountains"
+                />{" "}
+                <div class="px-6 py-4">
+                  <div class="font-bold text-xl mb-2">Jorge dumit</div>
+                  <p class="text-gray-700 text-base">chef</p>
+                </div>
+              </div>
+            </div>
+            <div className="lg:w-1/2 flex justify-center sm:pt-4">
+              <div class="max-w-sm rounded overflow-hidden shadow-lg">
+                <img
+                  className="w-64 h-64 object-cover"
+                  src="https://imagenesrutalab.s3.amazonaws.com/llorona/nextImage/Screenshot+2024-04-11+at+20.20.30.png"
+                  alt="Sunset in the mountains"
+                />{" "}
+                <div class="px-6 py-4">
+                  <div class="font-bold text-xl mb-2">rasnyshhdj</div>
+                  <p class="text-gray-700 text-base">dj</p>
+                </div>
+              </div>
+            </div>
+          </div>{" "}
+        </>
+      ) : (
+        <>
+          <NavBar />
+          <div className="pb-4">
+            <div
+              className="d-flex subContenedor1Banner"
+              style={{
+                backgroundImage: `url("https://imagenesrutalab.s3.amazonaws.com/llorona/nextImage/aguacatechicharronrobeye.jpg")`,
+                backgroundSize: "cover",
+                height: "80vh",
+                backgroundPosition: "right center",
+                position: "relative",
+              }}
+            ></div>
+          </div>
+          <div className="container-llorona">
+            <div>
+              <h4 className="text-3xl text-center font-bold uppercase">
+                ROAD TO OPENING
+              </h4>
+            </div>
+          </div>
+
+          <div className="flex flex-col lg:flex-row lg:items-center lg:justify-center lg:p-8">
+            <div className="md:w-1/4">
+              <p className="w-full max-w-[1000px] mx-auto text-center md:text-justify text-base md:text-lg text-black font-bold">
+                First Act
+              </p>
+              <p className="w-full max-w-[1000px]  mx-auto text-center md:text-justify text-base md:text-lg text-black">
+                Sunday 14 abril 2024. <br />
+                5:00 PM A 9:00 PM
+              </p>
+            </div>
+            <div className="md:w-3/4">
+              {" "}
+              <p className="w-full max-w-[800px]  mx-auto text-justify sm:text-2xl text-xl text-black text-bold sm:pl-4 sm:text-lg text-sm pt-2 px-2">
+                We are pleased to invite you to taste dishes that we have
+                selected for you from our new menu, accompanied by our guest
+                bartender and DJ rasnyshh.
+              </p>
+            </div>
+          </div>
+          <>
+            <div className="flex flex-col lg:flex-row lg:items-center lg:justify-between lg:p-8 mt-4 pb-2">
+              <div className="lg:w-1/2 flex justify-center pt-2">
+                <div class="max-w-sm rounded overflow-hidden shadow-lg">
+                  <img
+                    className="w-64 h-64 object-cover"
+                    src="https://imagenesrutalab.s3.amazonaws.com/llorona/nextImage/jorgedumit.jpg"
+                    alt="Sunset in the mountains"
+                  />{" "}
+                  <div class="px-6 py-4">
+                    <div class="font-bold text-xl mb-2">Jorge dumit</div>
+                    <p class="text-gray-700 text-base">chef</p>
+                  </div>
+                </div>
+              </div>
+              <div className="lg:w-1/2 flex justify-center pt-2">
+                <div class="max-w-sm rounded overflow-hidden shadow-lg">
+                  <img
+                    className="w-64 h-64 object-cover"
+                    src="https://imagenesrutalab.s3.amazonaws.com/llorona/nextImage/Screenshot+2024-04-11+at+20.20.30.png"
+                    alt="Sunset in the mountains"
+                  />{" "}
+                  <div class="px-6 py-4">
+                    <div class="font-bold text-xl mb-2">rasnyshhdj</div>
+                    <p class="text-gray-700 text-base">dj</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </>
+        </>
+      )}
+
+      {espa ? (
+        <div className="w-full bg-white">
+          <div className="w-full rounded bg-gray-200 max-w-[1184px] py-[80px] px-[20px] mx-auto">
+            <h1 className="text2xl md:text-5xl font-bold text-center text-gray-900">
+              Reserva ya y pide un postre especial del chef
+            </h1>
+
+            <InlineWidget url="https://calendly.com/reservaciones-14/googleads-roadopening-14-04-24" />
+          </div>
+        </div>
+      ) : (
+        <div className="w-full bg-white">
+          <div className="w-full rounded bg-gray-200 max-w-[1184px] py-[80px] px-[20px] mx-auto">
+            <h1 className="text2xl md:text-5xl font-bold text-center text-gray-900">
+              book now for a special dessert by the chef
+            </h1>
+
+            <InlineWidget url="https://calendly.com/reservaciones-14/googleads-roadopening-14-04-24" />
+          </div>
+        </div>
+      )}
+    </>
+  );
+}
+
+export default RoadopeningGoogleAds;
