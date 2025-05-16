f01cce226a944e488913c85697182e49d961597e
jampierzcode <jampierv127@gmail.com>
2025-05-16 14:25:10 -0500

    google tag manager update

--- pages/_document.js
+++ pages/_document.js
@@ -1,48 +1,37 @@
 import { Html, Head, Main, NextScript } from 'next/document';
-import { FB_PIXEL_ID } from '../lib/fpixel';
-import { GTM_ID } from '../lib/gtm';
-import Script from 'next/script';
-import * as fbq from '../lib/fpixel';
 
 export default function Document() {
   return (
-    <Html>
+    <Html lang="en">
       <Head>
-        <script
-          async
-          src={`https://www.googletagmanager.com/ns.html?id=${GTM_ID}`}
-        />
-
+        {/* Google Tag Manager - Script */}
         <script
           dangerouslySetInnerHTML={{
             __html: `
-                (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-                'https://www.googletagmanager.com/gtm.js?id=GTM-M746PDQ';f.parentNode.insertBefore(j,f);
-                })(window,document,'script','dataLayer','${GTM_ID}');
-              `,
+              (function(w,d,s,l,i){
+                w[l]=w[l]||[];
+                w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});
+                var f=d.getElementsByTagName(s)[0],
+                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';
+                j.async=true;
+                j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
+                f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-M746PDQ');
+            `,
           }}
         />
-        <Script strategy="afterInteractive" src="/public/facebookPixel.js" />
-        <noscript>
-          <img
-            height="1"
-            width="1"
-            style={{ display: 'none' }}
-            src={`https://www.facebook.com/tr?id=${FB_PIXEL_ID}&ev=PageView&noscript=1`}
-          />
-        </noscript>
       </Head>
       <body>
+        {/* Google Tag Manager - NoScript */}
         <noscript>
           <iframe
-            src={`https://www.googletagmanager.com/ns.html?id=${GTM_ID}`}
+            src="https://www.googletagmanager.com/ns.html?id=GTM-M746PDQ"
             height="0"
             width="0"
             style={{ display: 'none', visibility: 'hidden' }}
           ></iframe>
         </noscript>
+
         <Main />
         <NextScript />
       </body>
